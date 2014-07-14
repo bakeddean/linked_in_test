ccd267e7f60256acd01e63987e909ac129705263
Dean Woodward <dean.woodward@datacom.co.nz>
2014-07-14 17:15:09 +1200

    Add LIACompany model.

---
+++ Example/IOSLinkedInAPI-Podexample/IOSLinkedInAPI-Podexample/IOSLinkedInAPI-Podexample/Models/LIACompany.m
@@ -0,0 +1,13 @@
+//
+//  LIACompany.m
+//  IOSLinkedInAPI-Podexample
+//
+//  Created by Dean Woodward on 14/07/14.
+//  Copyright (c) 2014 Eyben Consult ApS. All rights reserved.
+//
+
+#import "LIACompany.h"
+
+@implementation LIACompany
+
+@end
